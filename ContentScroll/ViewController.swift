b9f01f56ffaba289441641cc6d8ad7a2cd8856e2
sachin nautiyal <sachinnautiyal00@gmail.com>
2020-04-20 10:44:05 +0530

    Initial Commit

---
+++ ContentScroll/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ContentScroll
+//
+//  Created by Sachin Nautiyal on 20/04/20.
+//  Copyright © 2020 Sachin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let pageWidth: Float =  240+25 // PagingWidth = CellWidth + Cell Space
+        let currentOffset: Float = Float(scrollView.contentOffset.x)
+        let targetOffset: Float = Float(targetContentOffset.pointee.x)
+        var newTargetOffset: Float = 0
+        if targetOffset > currentOffset {
+            newTargetOffset = ceilf(currentOffset / pageWidth) * pageWidth
+        }
+        else {
+            newTargetOffset = floorf(currentOffset / pageWidth) * pageWidth
+        }
+        if newTargetOffset < 0 {
+            newTargetOffset = 0
+        }
+        else if (newTargetOffset > Float(scrollView.contentSize.width)){
+            newTargetOffset = Float(Float(scrollView.contentSize.width))
+        }
+        
+        targetContentOffset.pointee.x = CGFloat(currentOffset)
+        scrollView.setContentOffset(CGPoint(x: CGFloat(newTargetOffset), y: scrollView.contentOffset.y), animated: true)
+    }
+    
+}
+
+extension ViewController : UIScrollViewDelegate {
+    
+}
